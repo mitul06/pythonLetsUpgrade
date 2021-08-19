9516e069421a297b1ba42fb29544b348e01a95d3
Mitul Sartanpara <62390582+mitul06@users.noreply.github.com>
2021-08-19 16:05:38 +0530

    day 1 assignment Python Zero to Hero Submitted
    An assignment I was solved and placed file.

---
+++ day1.py
@@ -0,0 +1,13 @@
+#assignment
+
+#day 1
+
+# Take two inputs and store in variable in x and y, find x to the power y.
+
+
+x = int(input("Enter a number for x : "))
+y = int(input("Enter a number for y : "))
+
+
+print("x to the power y : ", end="")
+print(x ** y)
